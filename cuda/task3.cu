96e4840938d4c98a0411d4b4ed91a5d5b3d30f87
Dmytro Karpenko <dmytro.karpenko@developex.net>
2024-10-14 22:43:06 +0300

    Polish CUDA 3 task

--- cuda/task3.cu
+++ cuda/task3.cu
@@ -1,13 +1,4 @@
 #include <iostream>
-#include <cuda.h>
-
-__inline__ __device__ int warpReduceSum(int val) {
-    // Full mask for active threads in a warp
-    for (int offset = warpSize / 2; offset > 0; offset /= 2) {
-        val += __shfl_down_sync(0xFFFFFFFF, val, offset);
-    }
-    return val;
-}
 
 const int blockSize = 32;
 
@@ -33,61 +24,49 @@ __global__ void sumBlockKernel(int *out, int *a, int n)
 	} else {
 		sharedData[tid] = 0;
 	}
-
 	__syncthreads();
-	for (int i = blockDim.x / 2;  i > 0; i /= 2) {
-		if (tid < i) {
-			sharedData[tid] += sharedData[tid + i];
-		}
 
-		__syncthreads();
+	int value = sharedData[tid];
+	for (int i = blockDim.x / 2;  i > 0; i /= 2) {
+		value += __shfl_down_sync(0xFFFFFFFF, value, i);
 	}
 
 	if (tid == 0) {
-		out[blockIdx.x] = sharedData[0];
+		out[blockIdx.x] = value;
 	}
 }
 
-void sumGpu(int *out, int *a, int n, int blockNum, int threadNum)
+void sumGpu(int *a, int n, int blockNum, int threadNum)
 {
-	sumBlockKernel<<<blockNum, threadNum>>>(out, a, n); //to don`t copy whole a array and keep it
-
-	blockNum = (blockNum + threadNum - 1) / threadNum;
-	n = (n + threadNum - 1) / threadNum;
-
 	while (n > 1) { 
-		sumBlockKernel<<<blockNum, threadNum>>>(out, out, n);
+		sumBlockKernel<<<blockNum, threadNum>>>(a, a, n);
 		
 		blockNum = (blockNum + threadNum - 1) / threadNum;
 		n = (n + threadNum - 1) / threadNum;
 	}
 }
 
-
 int main()
 {
-	constexpr auto N = 10000000;
+	constexpr auto N = 1000000000;
 
 	auto a = new int[N];
 	
-	for(int i = 0; i < N; i++) {
-		a[i] = 2;
+	for(int i = 100; i < N; i++) {
+		a[i] = i % 11;
 	}
 
 	int *d_a = nullptr;
-	int *d_out = nullptr;
 
 	cudaMalloc((void**)&d_a, sizeof(int) * N);
 	cudaMemcpy(d_a, a, sizeof(int) * N, cudaMemcpyHostToDevice);
 
 	const auto blockNumbers = (N + blockSize - 1) / blockSize;
 
-	cudaMalloc((void**)&d_out, sizeof(int) * blockNumbers);
-
-	sumGpu(d_out, d_a, N, blockNumbers, blockSize);
+	sumGpu(d_a, N, blockNumbers, blockSize);
 
 	int deviceResult = 0;
-	cudaMemcpy(&deviceResult, d_out, sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(&deviceResult, d_a, sizeof(int), cudaMemcpyDeviceToHost);
 
 	const auto hostResult = sum(a, N);
 
@@ -103,7 +82,6 @@ int main()
 	delete[] a;
 
 	cudaFree(d_a);
-	cudaFree(d_out);
 
 	return 0;
 }
